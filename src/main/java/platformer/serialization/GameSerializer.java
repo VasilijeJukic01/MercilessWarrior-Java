ee92795a2206ae15c56195923ff8a414798c0384
VasilijeJukic01 <vjukic9222rn@raf.rs>
2023-09-08 12:49:03 +0200

    Save file protection using AES

--- src/main/java/platformer/serialization/GameSerializer.java
+++ src/main/java/platformer/serialization/GameSerializer.java
@@ -6,28 +6,53 @@ import platformer.core.Account;
 import platformer.debug.logger.Logger;
 import platformer.debug.logger.Message;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import static platformer.constants.FilePaths.SAVE_PATH;
 
 public class GameSerializer implements Serializer<Account, List<Account>> {
 
-    private Gson gson = new Gson();
     private final List<String> saves = List.of("save1", "save2", "save3");
+    private static final String SECRET_KEY = "HPLXBALYYYZWNX";
+
+    private static final byte[] SALT = new byte[] {
+            (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78,
+            (byte) 0x90, (byte) 0xAB, (byte) 0xCD, (byte) 0xEF
+    };
+    private static final byte[] IV = new byte[] {
+            (byte) 0x01, (byte) 0x23, (byte) 0x45, (byte) 0x67,
+            (byte) 0x89, (byte) 0xAB, (byte) 0xCD, (byte) 0xEF,
+            (byte) 0xFE, (byte) 0xDC, (byte) 0xBA, (byte) 0x98,
+            (byte) 0x76, (byte) 0x54, (byte) 0x32, (byte) 0x10
+    };
 
     @Override
     public void serialize(Account account, int index) {
-        try (FileWriter writer = new FileWriter(SAVE_PATH + "save"+index)) {
-            gson.toJson(account, writer);
-            Logger.getInstance().notify("Game saved to slot "+index+".", Message.NOTIFICATION);
+        try {
+            Gson gson = new Gson();
+            String jsonCache = gson.toJson(account);
+
+            String encryptedJson = encrypt(jsonCache);
+            if (encryptedJson == null) return;
+
+            try (FileWriter writer = new FileWriter(SAVE_PATH + "save" + index)) {
+                writer.write(encryptedJson);
+                Logger.getInstance().notify("Game saved to slot " + index + ".", Message.NOTIFICATION);
+            }
         }
         catch (IOException e) {
-            e.printStackTrace();
+            Logger.getInstance().notify("Game saving to slot " + index + "failed.", Message.ERROR);
         }
     }
 
@@ -41,19 +66,82 @@ public class GameSerializer implements Serializer<Account, List<Account>> {
             if (file.exists()) {
                 try {
                     FileReader fileReader = new FileReader(filePath);
-                    gson = new GsonBuilder().create();
+                    StringBuilder sb = new StringBuilder();
+                    int ch;
 
-                    Account account = gson.fromJson(fileReader, Account.class);
+                    while ((ch = fileReader.read()) != -1) {
+                        sb.append((char) ch);
+                    }
+                    fileReader.close();
+
+                    String encryptedJson = sb.toString();
+                    String decryptedJson = decrypt(encryptedJson);
+
+                    Gson gson = new GsonBuilder().create();
+                    Account account = gson.fromJson(decryptedJson, Account.class);
                     savedFilesData.add(account);
 
-                    fileReader.close();
                 }
                 catch (IOException e) {
-                    e.printStackTrace();
+                    Logger.getInstance().notify("Game at "+ save +" corrupted.", Message.ERROR);
                 }
             }
         }
         return savedFilesData;
+    }
+
+    private String encrypt(String data) {
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT, 65536, 256);
+            SecretKey temp = factory.generateSecret(spec);
+            SecretKey secret = new SecretKeySpec(temp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secret, new IvParameterSpec(IV));
+
+            byte[] encryptedText = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+
+            byte[] saltAndIv = new byte[SALT.length + IV.length];
+            System.arraycopy(SALT, 0, saltAndIv, 0, SALT.length);
+            System.arraycopy(IV, 0, saltAndIv, SALT.length, IV.length);
+
+            byte[] encryptedData = new byte[saltAndIv.length + encryptedText.length];
+            System.arraycopy(saltAndIv, 0, encryptedData, 0, saltAndIv.length);
+            System.arraycopy(encryptedText, 0, encryptedData, saltAndIv.length, encryptedText.length);
+
+            return Base64.getEncoder().encodeToString(encryptedData);
+        }
+        catch (Exception ignored) {}
+
+        return null;
+    }
+
+    private String decrypt(String data) {
+        try {
+            byte[] encryptedData = Base64.getDecoder().decode(data);
+
+            byte[] saltAndIv = new byte[SALT.length + IV.length];
+            System.arraycopy(encryptedData, 0, saltAndIv, 0, saltAndIv.length);
+
+            byte[] encryptedText = new byte[encryptedData.length - saltAndIv.length];
+            System.arraycopy(encryptedData, saltAndIv.length, encryptedText, 0, encryptedText.length);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT, 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(IV));
+
+            byte[] decryptedText = cipher.doFinal(encryptedText);
+
+            return new String(decryptedText, StandardCharsets.UTF_8);
+        }
+        catch (Exception ignored) {}
+
+        return null;
     }
 
 }
