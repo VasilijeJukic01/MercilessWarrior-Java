2aec993a7acc6099df013a0f8a87522288f7501f
Vasilije Jukic <vasilijejukic@gmail.com>
2025-08-03 10:55:32 +0200

    Javadoc some classes

--- core/src/main/java/platformer/observer/Publisher.java
+++ core/src/main/java/platformer/observer/Publisher.java
@@ -1,11 +1,34 @@
 package platformer.observer;
 
+/**
+ * Defines the contract for the 'Publisher' in the Observer design pattern.
+ * <p>
+ * A Publisher maintains a list of subscribers and provides a mechanism to notify them of any state changes or events.
+ *
+ * @see Subscriber
+ */
 public interface Publisher {
 
+    /**
+     * Registers a new Subscriber to receive notifications from this Publisher.
+     *
+     * @param s The Subscriber to add to the notification list.
+     */
     void addSubscriber(Subscriber s);
 
+    /**
+     * Removes a Subscriber from the notification list. After being removed, the Subscriber will no longer receive updates from this Publisher.
+     *
+     * @param s The Subscriber to remove from the notification list.
+     */
     void removeSubscriber(Subscriber s);
 
+    /**
+     * Broadcasts an event to all registered subscribers by calling their {@code update} method.
+     *
+     * @param <T> The type of the data being passed in the event.
+     * @param o   A list containing the event data. The structure of this data is dependent on the specific implementation and event type.
+     */
     <T> void notify(T ... o);
 
 }
--- core/src/main/java/platformer/observer/Subscriber.java
+++ core/src/main/java/platformer/observer/Subscriber.java
@@ -1,7 +1,21 @@
 package platformer.observer;
 
+/**
+ * Defines the contract for the 'Subscriber' in the Observer design pattern.
+ * <p>
+ * A Subscriber registers with a {@link Publisher} to receive notifications about state changes or events.
+ * When an event occurs, the Publisher calls the {@code update} method on all of its registered subscribers.
+ *
+ * @see Publisher
+ */
 public interface Subscriber {
 
+    /**
+     * This method is called by a {@link Publisher} to notify the Subscriber of an event.
+     *
+     * @param <T> The type of the data being passed in the event.
+     * @param o   A list containing the event data sent by the Publisher. Subscribers must know the expected structure of this data to correctly process the event.
+     */
     <T> void update(T ... o);
 
 }
